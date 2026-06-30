e115741e818fa00c7c0ad1bba6b2bc3e69960042
James Smith <james@floppy.org.uk>
2026-06-30 17:36:31 +0100

    fix file_url generator method

--- lib/open_local_files.rb
+++ lib/open_local_files.rb
@@ -7,8 +7,8 @@ module OpenLocalFiles
     library_path = ENV.fetch("OPEN_LOCAL_FILES_LIBRARY_PATH", fallback_library_path)
     return nil unless library_path
 
-    path = File.join(library_path, file.path_within_library)
-    URI::File.build(path: path.split(File::SEPARATOR).map {CGI.escapeURIComponent it}.join("/")).to_s
+    local_path = File.join(library_path, path)
+    URI::File.build(path: local_path.split(File::SEPARATOR).map {CGI.escapeURIComponent it}.join("/")).to_s
   end
 
 end
