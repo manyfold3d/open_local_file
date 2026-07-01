1fdcd0e2c243f00de4e72971a84d5b3d98695c14
James Smith <james@floppy.org.uk>
2026-07-01 16:50:06 +0100

    minor format linting

--- lib/open_local_files.rb
+++ lib/open_local_files.rb
@@ -2,13 +2,11 @@ require "open_local_files/version"
 require "open_local_files/engine"
 
 module OpenLocalFiles
-
   def self.file_url_for(path:, fallback_library_path: nil)
     library_path = ENV.fetch("OPEN_LOCAL_FILES_LIBRARY_PATH", fallback_library_path)
     return nil unless library_path
 
     local_path = File.join(library_path, path)
-    URI::File.build(path: local_path.split(File::SEPARATOR).map {CGI.escapeURIComponent it}.join("/")).to_s
+    URI::File.build(path: local_path.split(File::SEPARATOR).map { CGI.escapeURIComponent it }.join("/")).to_s
   end
-
 end
